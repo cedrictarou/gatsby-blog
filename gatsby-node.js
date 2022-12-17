dcb360cd6e82756f5d1a41e3c288bdbe7f7a7cfd
cedrictaro-g <cedric.tarou@gmail.com>
2022-12-18 07:06:47 +0900

    created blog index page with pagenation

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,4 +40,21 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       });
     }
   );
+  const blogPostsPerPage = 6;
+  const blogPosts = blogresult.data.allContentfulBlogPost.edges.length;
+  const blogPages = Math.ceil(blogPosts / blogPostsPerPage);
+
+  Array.from({ length: blogPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog/` : `blog/${i + 1}/`,
+      component: path.resolve("./src/templates/blog-template.js"),
+      context: {
+        skip: blogPostsPerPage * i,
+        limit: blogPostsPerPage,
+        currentPage: i + 1,
+        isFirst: i + 1 === 1,
+        isLast: i + 1 === blogPages,
+      },
+    });
+  });
 };
---
+++ src/templates/blog-template.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Layout from "../components/layout";
+import { graphql, Link } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+import Seo from "../components/seo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+export default ({ data, location, pageContext }) => (
+  <Layout>
+    <Seo
+      pagetitle='ブログ'
+      pagedesc='ESSENTIALSのブログです'
+      patepath={location.pathname}
+    />
+    <section className='content bloglist'>
+      <div className='container'>
+        <h1 className='bar'>RECENT POSTS</h1>
+        <div className='posts'>
+          {data.allContentfulBlogPost.edges.map(({ node }) => (
+            <article className='post' key={node.id}>
+              <Link to={`/blog/post/${node.slug}/`}>
+                <figure>
+                  <GatsbyImage
+                    image={node.eyecatch.gatsbyImageData}
+                    style={{ height: "100%" }}
+                    alt={node.description}
+                  />
+                </figure>
+                <h3>{node.title}</h3>
+              </Link>
+            </article>
+          ))}
+        </div>
+
+        <ul className='pagenation'>
+          {!pageContext.isFirst && (
+            <li className='prev'>
+              <Link
+                to={
+                  pageContext.currentPage === 2
+                    ? `/blog/`
+                    : `/blog/${pageContext.currentPage - 1}`
+                }
+                rel='prev'
+              >
+                <FontAwesomeIcon icon={faChevronLeft} />
+                <span>前のページ</span>
+              </Link>
+            </li>
+          )}
+          {!pageContext.isLast && (
+            <li className='next'>
+              <Link to={`/blog/${pageContext.currentPage + 1}`} rel='next'>
+                <span>次のページ</span>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </section>
+  </Layout>
+);
+
+export const query = graphql`
+  query ($skip: Int!, $limit: Int!) {
+    allContentfulBlogPost(
+      sort: { publishDate: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          title
+          id
+          eyecatch {
+            gatsbyImageData(layout: CONSTRAINED, width: 500)
+            description
+          }
+          slug
+        }
+      }
+    }
+  }
+`;
